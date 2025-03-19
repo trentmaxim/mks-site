0b3f684e2d4bfc407e6b245c006b0caed9377a51
SreekumarCNambiar <sreekumars4123@gmail.com>
2025-03-19 21:57:08 +0530

    minor fixes

--- src/app/components/main-navbar.js
+++ src/app/components/main-navbar.js
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
-import Link from 'next/link';
-import '@/app/globals.css';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import "@/app/globals.css";
 
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -29,20 +29,25 @@ export default function NavBar() {
       lastScrollY = currentScrollY;
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <nav className={` main-navbar sticky top-0 z-50 bg-white text-black dark:bg-black dark:text-white transition-all duration-300
-    ${isNavTopVisible ? '' : 'top-[-5%]'}`}>
+    <nav
+      className={`flex flex-col items-center justify-center main-navbar sticky top-0 z-50 bg-white text-black dark:bg-black dark:text-white transition-all duration-300 ${
+        isNavTopVisible ? "" : "top-[-5%]"
+      }`}
+    >
       <div
-        className={`flex items-center md:justify-center sm:justify-between px-4 py-3 border-b border-gray-300 w-full nav-top transform transition-all duration-500 ease-in-out ${
-          isNavTopVisible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
-        }`}
+        className={`flex items-center px-4 py-3 w-full nav-top transform transition-all duration-500 ease-in-out ${
+          isNavTopVisible
+            ? "translate-y-0 opacity-100"
+            : "-translate-y-full opacity-0"
+        } justify-between md:justify-center`}
       >
         {/* Logo */}
         <h1 className="text-2xl font-bold">Malayala Kshatriya Samajam</h1>
@@ -56,10 +61,12 @@ export default function NavBar() {
         </button>
       </div>
 
+      <div className="h-[1px] w-3/4 bg-white"></div>
+
       {/* Navigation Links */}
       <div
         className={`md:flex md:items-center md:justify-center ${
-          isMenuOpen ? 'block' : 'hidden'
+          isMenuOpen ? "block" : "hidden"
         }`}
       >
         <ul className="flex flex-col md:flex-row md:space-x-6 text-center">
